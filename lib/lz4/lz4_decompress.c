a1cc66a8cb2f7483c4c2f38f2de3ac6dc3eef329
Colin Ian King <colin.king@canonical.com>
2017-10-03 16:16:01 -0700

    lib/lz4: make arrays static const, reduces object code size
    Don't populate the read-only arrays dec32table and dec64table on the
    stack, instead make them both static const.  Makes the object code
    smaller by over 10K bytes:
    
      Before:
         text	   data	    bss	    dec	    hex	filename
        31500	      0	      0	  31500	   7b0c	lib/lz4/lz4_decompress.o
    
      After:
         text	   data	    bss	    dec	    hex	filename
        20237	    176	      0	  20413	   4fbd	lib/lz4/lz4_decompress.o
    
    (gcc version 7.2.0 x86_64)
    
    Link: http://lkml.kernel.org/r/20170921221939.20820-1-colin.king@canonical.com
    Signed-off-by: Colin Ian King <colin.king@canonical.com>
    Cc: Christophe JAILLET <christophe.jaillet@wanadoo.fr>
    Cc: Sven Schmidt <4sschmid@informatik.uni-hamburg.de>
    Cc: Arnd Bergmann <arnd@arndb.de>
    Cc: Joe Perches <joe@perches.com>
    Signed-off-by: Andrew Morton <akpm@linux-foundation.org>
    Signed-off-by: Linus Torvalds <torvalds@linux-foundation.org>
    Signed-off-by: Jprimero15 <jprimero155@gmail.com>
    Signed-off-by: Risti699 <wahhap10@gmail.com>

--- lib/lz4/lz4_decompress.c
+++ lib/lz4/lz4_decompress.c
@@ -59,8 +59,12 @@ static int lz4_uncompress(const char *source, char *dest, int osize)
 	BYTE *op = (BYTE *) dest;
 	BYTE * const oend = op + osize;
 	BYTE *cpy;
-	unsigned token;
-	size_t length;
+	BYTE *oexit = op + targetOutputSize;
+	const BYTE * const lowLimit = lowPrefix - dictSize;
+
+	const BYTE * const dictEnd = (const BYTE *)dictStart + dictSize;
+	static const unsigned int dec32table[] = { 0, 1, 2, 1, 4, 4, 4, 4 };
+	static const int dec64table[] = { 0, 0, 0, -1, 0, 1, 2, 3 };
 
 	while (1) {
 
