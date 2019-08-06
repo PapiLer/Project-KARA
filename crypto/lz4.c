a1c8436c5fe224f39b145a1dbf7371dd8d7fe848
Angelo G. Del Regno <kholk11@gmail.com>
2019-08-06 18:33:22 +0200

    crypto: Merge: Introduce scompress and acompress APIs.
    This was taken from
    Repo:
    https://github.com/sonyxperiadev/kernel
    PR: 1935 ([2.3.2.r1.4] Upgrade and stabilize ZRAM with Z3FOLD, LZ4)
    
    and is used in preparation of the next performance improvements.
    
    Signed-off-by: Jprimero15 <jprimero155@gmail.com>
    Signed-off-by: Risti699 <wahhap10@gmail.com>

--- crypto/lz4.c
+++ crypto/lz4.c
@@ -58,8 +58,29 @@ static int lz4_compress_crypto(struct crypto_tfm *tfm, const u8 *src,
 	return 0;
 }
 
-static int lz4_decompress_crypto(struct crypto_tfm *tfm, const u8 *src,
-			      unsigned int slen, u8 *dst, unsigned int *dlen)
+static int lz4_scompress(struct crypto_scomp *tfm, const u8 *src,
+			 unsigned int slen, u8 *dst, unsigned int *dlen,
+			 void *ctx)
+{
+	return __lz4_compress_crypto(src, slen, dst, dlen, ctx);
+}
+
+static int lz4_compress_crypto(struct crypto_tfm *tfm, const u8 *src,
+			       unsigned int slen, u8 *dst, unsigned int *dlen)
+{
+	struct lz4_ctx *ctx = crypto_tfm_ctx(tfm);
+	int out_len = LZ4_compress_default(src, dst,
+		slen, *dlen, ctx->lz4_comp_mem);
+
+	if (!out_len)
+                return -EINVAL;
+
+        *dlen = out_len;
+        return 0;
+}
+
+static int __lz4_decompress_crypto(const u8 *src, unsigned int slen,
+				   u8 *dst, unsigned int *dlen, void *ctx)
 {
 	int out_len = LZ4_decompress_safe(src, dst, slen, *dlen);
 
@@ -68,6 +89,26 @@ static int lz4_decompress_crypto(struct crypto_tfm *tfm, const u8 *src,
 
 	*dlen = out_len;
 	return 0;
+}
+
+static int lz4_sdecompress(struct crypto_scomp *tfm, const u8 *src,
+			   unsigned int slen, u8 *dst, unsigned int *dlen,
+			   void *ctx)
+{
+	int out_len = LZ4_decompress_safe(src, dst, slen, *dlen);
+
+	if (out_len < 0)
+		return -EINVAL;
+
+	*dlen = out_len;
+	return 0;
+}
+
+static int lz4_decompress_crypto(struct crypto_tfm *tfm, const u8 *src,
+				 unsigned int slen, u8 *dst,
+				 unsigned int *dlen)
+{
+	return __lz4_decompress_crypto(src, slen, dst, dlen, NULL);
 }
 
 static struct crypto_alg alg_lz4 = {
